7a04cdd17c51a0ec2ae9bb7aad47d49ae8a92ff6
Meghan-Savage <MSavage1@protonmail.com>
2023-03-14 12:49:29 -0600

    made a poisoned function, coded next set of choices in the story

--- functions/text-functions.js
+++ functions/text-functions.js
@@ -6,4 +6,8 @@ function dangerText(string) {
   return console.log(`\x1B[31m${string}\x1B[0m`);
 }
 
-module.exports = { dangerText, youDiedText };
+function spiderText(string) {
+  return console.log(`\x1b[42m\x1b[30m${string}\x1b[0m`);
+}
+
+module.exports = { dangerText, youDiedText, spiderText };
